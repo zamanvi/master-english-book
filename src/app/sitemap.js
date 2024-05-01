6d638f44b39e163052798dc4a79d34c885994f6f
developer-shourav <mrinmoyraj823@gmail.com>
2024-05-01 13:56:26 +0600

    Sitemap added

---
+++ src/app/sitemap.js
@@ -0,0 +1,45 @@
+import getBook from "../../lib/getBook";
+import getChapterData from "../../lib/getChapterData";
+
+export const revalidate = 30
+
+export default async function sitemap() {
+
+    const siteURL = process.env.NEXT_WEBSITE_URL;
+    const bookData = await getBook();
+    const allChapters = bookData?.success?.data?.chapters?.data;
+    let dynamicPages = [];
+
+    await Promise.all(
+      allChapters?.map(async (chapter) => {
+
+        const chapterData = await getChapterData(chapter.id);
+        const chapterContent = chapterData?.success?.data?.items?.data;
+
+        chapterContent?.map((content) => {
+
+          const postPage = {
+            url: `${siteURL}/book/${content?.id}`,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+          };
+
+          dynamicPages.push(postPage);
+        });
+      })
+    );
+  
+    return [
+      {
+        url: siteURL,
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 1,
+      },
+      ...dynamicPages
+      
+    ]
+  }
+    
+
