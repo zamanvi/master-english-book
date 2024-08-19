ee041df664ac78b381fde5d7f8ea2d1040378bf0
developer-shourav <mrinmoyraj823@gmail.com>
2024-08-19 11:10:37 +0600

    Books all issues solved

--- src/app/components/BookCover/BookCover.jsx
+++ src/app/components/BookCover/BookCover.jsx
@@ -6,22 +6,21 @@ import Link from "next/link";
 const BookCover = () => {
   return (
     <div className="w-11/12 md:w-10/12 lg:w-full mx-auto pt-5 ">
-      <Link href="../WordAudio">
-        <Image
-          src={coverPhoto}
-          className="mx-auto md:w-7/12 lg:w-5/12  rounded-md shadow-md shadow-[#0000008f]"
-          alt="English Book Cover"
-        />
-      </Link>
+      <Image
+        src={coverPhoto}
+        className="mx-auto md:w-7/12 lg:w-5/12  rounded-md shadow-md shadow-[#0000008f]"
+        alt="English Book Cover"
+      />
 
       <Link
         href="https://play.google.com/store/apps/details?id=com.abmn.englishhub&pcampaignid=web_share"
-        className="flex justify-center items-center gap-x-2 my-5"
+        className="flex flex-col justify-center items-center gap-y-4 my-5"
       >
-        <span className="font-semibold text-lg text-blue-500">
+        
+        <Image className="rounded-md" width={55} src={appLogo} alt="App logo" />
+        <span className="font-semibold text-xl md:text-2xl text-blue-500">
           Download Our App
         </span>
-        <Image className="rounded-md" width={55} src={appLogo} alt="App logo" />
       </Link>
     </div>
   );
