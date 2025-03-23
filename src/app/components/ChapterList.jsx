27275fa00b05b7126c766720de82d01c916c3b1d
developer-shourav <mrinmoyraj823@gmail.com>
2025-03-23 17:23:13 +0600

    Active Link Added

--- src/app/components/ChapterList.jsx
+++ src/app/components/ChapterList.jsx
@@ -1,9 +1,11 @@
-'use client';
+"use client";
 import { useState } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation"; 
 
 const ChapterList = ({ chapter, chapterContent }) => {
   const [showAll, setShowAll] = useState(false);
+  const pathname = usePathname(); // Get current URL path
   const visibleContent = showAll ? chapterContent : chapterContent.slice(0, 20);
 
   return (
@@ -11,19 +13,24 @@ const ChapterList = ({ chapter, chapterContent }) => {
       <h3 className="text-[15px] lg:text-[16px] font-semibold mb-1 lg:mb-2">
         {chapter?.title}
       </h3>
-      {/* -----------Display limited chapter content initially----------- */}
-      {visibleContent?.map((content, index) => (
-        <Link
-          key={content?.slug}
-          className="cursor-pointer block mb-3 xl:mb-4 hover:text-blue-400 text-[16px]"
-          href={`../book/${content?.slug}`}
-          title={content?.title}
-        >
-          <span className="font-bold">{index + 1}. </span> {content?.title}
-        </Link>
-      ))}
+      {/* ----------- Display limited chapter content initially ----------- */}
+      {visibleContent?.map((content, index) => {
+        const isActive = pathname === `/book/${content?.slug}`;
 
-      {/* -----------"See More" Button----------- */}
+        return (
+          <Link
+            key={content?.slug}
+            className={`cursor-pointer block mb-3 xl:mb-4 text-[16px] hover:text-blue-500 
+              ${isActive ? "text-blue-500 font-semibold" : ""}`}
+            href={`../book/${content?.slug}`}
+            title={content?.title}
+          >
+            <span className="font-bold">{index + 1}. </span> {content?.title}
+          </Link>
+        );
+      })}
+
+      {/* ----------- "See More" Button ----------- */}
       {!showAll && chapterContent.length > 20 && (
         <button
           className="text-blue-500 hover:text-blue-700 font-semibold mb-2"
@@ -32,8 +39,9 @@ const ChapterList = ({ chapter, chapterContent }) => {
           See More
         </button>
       )}
-      {/*----------- "See Less" Button -----------*/}
-      {showAll &&  (
+
+      {/* ----------- "See Less" Button ----------- */}
+      {showAll && (
         <button
           className="text-blue-500 hover:text-blue-700 font-semibold mb-2"
           onClick={() => setShowAll(false)}
