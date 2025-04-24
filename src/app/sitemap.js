8cff6db194564347901fdbb6e4b603b974f61891
developer-shourav <mrinmoyraj823@gmail.com>
2025-04-24 22:24:58 +0600

    Sitemap updated

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,45 +1,51 @@
 import getBook from "../../lib/getBook";
 import getChapterData from "../../lib/getChapterData";
 
-export const revalidate = 30
+export const revalidate = 30;
 
 export default async function sitemap() {
+  const siteURL = process.env.NEXT_WEBSITE_URL;
+  const bookData = await getBook();
+  const allChapters = bookData?.success?.data?.chapters?.data;
+  let dynamicPages = [];
 
-    const siteURL = process.env.NEXT_WEBSITE_URL;
-    const bookData = await getBook();
-    const allChapters = bookData?.success?.data?.chapters?.data;
-    let dynamicPages = [];
+  await Promise.all(
+    allChapters?.map(async (chapter) => {
+      const chapterData = await getChapterData(chapter.slug);
+      const chapterContent = chapterData?.success?.data?.items?.data;
 
-    await Promise.all(
-      allChapters?.map(async (chapter) => {
+      chapterContent?.map((content) => {
+        const postPage = {
+          url: `${siteURL}/book/${content?.slug}`,
+          lastModified: new Date(),
+          changeFrequency: "weekly",
+          priority: 0.8,
+        };
 
-        const chapterData = await getChapterData(chapter.slug);
-        const chapterContent = chapterData?.success?.data?.items?.data;
+        dynamicPages.push(postPage);
+      });
+    })
+  );
 
-        chapterContent?.map((content) => {
-
-          const postPage = {
-            url: `${siteURL}/book/${content?.slug}`,
-            lastModified: new Date(),
-            changeFrequency: "weekly",
-            priority: 0.8,
-          };
-
-          dynamicPages.push(postPage);
-        });
-      })
-    );
-  
-    return [
-      {
-        url: siteURL,
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 1,
-      },
-      ...dynamicPages
-      
-    ]
-  }
-    
-
+  return [
+    {
+      url: siteURL,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    ...dynamicPages,
+    {
+      url: `${siteURL}/TermsAndConditions`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: `${siteURL}/PrivacyPolicy`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
+}
